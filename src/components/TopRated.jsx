fb7082ee9206f33bcc98b771fbd7c523d6ed3881
TapiwaHerbetNosenga <144127869+TapiwaHerbetNosenga@users.noreply.github.com>
2025-03-28 15:09:38 +0200

    Landing-page: item-row-on

---
+++ src/components/TopRated.jsx
@@ -0,0 +1,12 @@
+
+
+export default function TopRated(){
+    return(
+      <div className="flex flex-col justify-center bg-green-300">
+    <div>
+        <p>Top Rated</p>
+
+    </div>
+      </div>  
+    );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,30 @@
+import Carousal from "../components/Carousal";
+import TopRated from "../components/TopRated";
+import OurPicks from "../components/OurPicks";
 
+export default function HomePage() {
+  return (
+    <div className="bg-green-700 flex flex-col justify-center ">
+     
+      {/* first row of landing page */}
+      <div className="grid grid-cols-5">
 
-export default function HomePage(){
-    return(
-        <div className="bg-gre">
-
+        {/* Carousal */}
+        {/* Width scrolling started here from the col span attribute */}
+        <div className="col-span-3">
+          <Carousal />
         </div>
-    );
+
+        {/* Top rated */}
+        <div className="col-span-2">
+          <TopRated />
+        </div>
+      </div>
+      
+      <div className="">
+          <OurPicks />
+        </div>
+    
+    </div>
+  );
 }
